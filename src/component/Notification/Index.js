8e74a2ed9ff044b18eb3033a308178fac5e6ab6f
Ada <wb-xyl390954@alibaba-inc.com>
2019-04-18 15:37:49 +0800

    api表格组件

--- src/component/Notification/Index.js
+++ src/component/Notification/Index.js
@@ -6,27 +6,45 @@ import Button from '../Button/Button'
 
 const component = () => {
     const onSuccess = (position) => {
-        Toast.success({ msg: 'success ', position})
+        Toast.success({ msg: 'success ', position })
     }
     const onFail = () => {
-        Toast.fail({ 
+        Toast.fail({
             msg: '错误提示!!',
             duration: 1
-         })
+        })
     }
     const onWarning = () => {
         Toast.warning({
             msg: '警告!'
         })
     }
-    const detail = `
-        import Notification from './Notification'
-        import Button from '../Button/Button';
+    const intrudction = {
+        detail: `
+        import { Notification } from 'three-work-ui'
         Notification.success({ msg: 'success !!!' })
         Notification.fail({ 
             msg: 'fail !!!',
             duration: 1
-        })`
+        })`,
+        apis: {
+            title: 'Notification',
+            values:[
+                {
+                    property: 'Notification.api',
+                    summary: '通知类型，可选值：success,warning,error,fail',
+                    _type_: 'String',
+                    defaultValue: '必选'
+                },
+                {
+                    property: 'api({ msg, position, duration })',
+                    summary: 'msg: 通知消息，position: 通知位置，duration: 通知时长',
+                    _type_: 'String,String,Number',
+                    defaultValue: 'success, right, 3'
+                }
+            ]
+        }
+    }
     const CaseList = [
         () => <Button onClick={onSuccess}>成功类通知</Button>,
         () => <Button onClick={onWarning} type="warning">警告类通知</Button>,
@@ -34,10 +52,10 @@ const component = () => {
         () => <Button onClick={() => onSuccess('top')}>top</Button>,
         () => <Button onClick={() => onSuccess('left')}>left</Button>,
     ]
-    return(
+    return (
         <div>
             <CaseContainer CaseList={CaseList} />
-            <Intrudction detail={detail}/>
+            <Intrudction intrudction={intrudction} />
         </div>
     )
 }
