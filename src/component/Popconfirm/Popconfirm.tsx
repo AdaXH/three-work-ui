aac11162127afe086a38919b28b1db743bb0057e
Ada <wb-xyl390954@alibaba-inc.com>
2019-04-26 14:11:27 +0800

    refactor: ts - config

--- src/component/Popconfirm/Popconfirm.tsx
+++ src/component/Popconfirm/Popconfirm.tsx
@@ -2,7 +2,23 @@ import React from 'react';
 import { mountComponent } from '../WrapComponent/Index'
 import './index.css'
 
-const Popconfirm = props => {
+export interface PopconfirmState {
+    visible: boolean,
+    show: boolean,
+    timeout: any
+}
+
+export interface PopconfirmProps {
+    children?: React.ReactNode,
+    onConfirm?: () => void,
+    onCancel?: () => void,
+    okText?: React.ReactNode,
+    cancelText?: React.ReactNode,
+    visible?: boolean,
+    title?: React.ReactNode
+}
+
+const Popconfirm = (props: PopconfirmProps) => {
     return (
         <div>
             <div className='TW_UI_popConfirmChildren' onClick={(e) => openPopconfirmContainer(e, props)}>
@@ -12,14 +28,10 @@ const Popconfirm = props => {
     )
 }
 
-const openPopconfirmContainer = (e, props) => {
-    const extendsProps = {
-        onConfirm: () => console.log('onConfirm'),
-        onCancel: () => console.log('onCancel'),
-        ...props,
-        visible: true,
-    }
-    // console.log(offsetLeft)
+const openPopconfirmContainer = (e: React.MouseEvent<HTMLDivElement, MouseEvent>, props: PopconfirmProps) => {
+    e.stopPropagation()
+    e.preventDefault()
+
     const left = e.clientX
     const top = e.clientY
     const MAX_TOP = 110
@@ -29,21 +41,30 @@ const openPopconfirmContainer = (e, props) => {
         top: top - MAX_TOP + 'px'
     }
 
-    class PopConfirm extends React.PureComponent {
-        state = {
-            visible: extendsProps.visible || false,
+    class PopConfirm extends React.Component<PopconfirmProps, PopconfirmState> {
+
+        static extendsProps: PopconfirmProps = {
+            onConfirm: () => console.log('onConfirm'),
+            onCancel: () => console.log('onCancel'),
+            title: 'delete title',
+            okText: 'ok',
+            cancelText: 'no',
+            visible: true,
+            ...props
+        }
+
+        state: PopconfirmState = {
+            visible: PopConfirm.extendsProps.visible || false,
             show: false,
-            timeout: null
+            timeout: () => void 0
         }
 
-        close = () => {
+        handleClick = (_type_: string) => {
             this.setState({ show: false }, () => setTimeout(() => this.setState({ visible: false }), 300))
-            extendsProps.onCancel()
-        }
-
-        submit = () => {
-            this.setState({ show: false }, () => setTimeout(() => this.setState({ visible: false }), 300))
-            extendsProps.onConfirm()
+            _type_ === 'close' ? 
+                PopConfirm.extendsProps.onCancel && PopConfirm.extendsProps.onCancel()
+            : 
+                PopConfirm.extendsProps.onConfirm && PopConfirm.extendsProps.onConfirm()
         }
 
         componentDidMount() {
@@ -61,17 +82,17 @@ const openPopconfirmContainer = (e, props) => {
             const className = show ? 'TW_UI_popconfirmShow' : 'TW_UI_popconfirmHide'
             return (
                 <div>
-                    <div className='TW_UI_popconfirmMask' onClick={this.close} />
+                    <div className='TW_UI_popconfirmMask' onClick={() => this.handleClick('close')} />
                     <div className={'TW_UI_popconfirmContainer ' + className} style={{ ...styles }}>
                         <div className='_TW_UI_popconfirmContext'>
                             <span className='TW_UI_popconfirmIcon'>
                                 <i className='icon-yiwen iconfont'></i>
                             </span>
-                            {props.title || 'Are you sure to delete ?'}
+                            {PopConfirm.extendsProps.title}
                         </div>
                         <div className='TW_UI_popconfirmFooter'>
-                            <span onClick={this.close}>{props.cancelText || 'cancel'}</span>
-                            <span onClick={this.submit}>{props.okText || 'ok'}</span>
+                            <span onClick={() => this.handleClick('close')}>{PopConfirm.extendsProps.cancelText}</span>
+                            <span onClick={() => this.handleClick('submit')}>{PopConfirm.extendsProps.okText}</span>
                         </div>
                     </div>
                 </div>
